44c6f85b44dc1f2eb5b5911e37b4006cbd9aa9c0
vanlucphan <vanlucbs1997@gmail.com>
2021-09-08 22:22:54 +0700

    config redis and sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://localhost:6379/0" }
+  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
 
   schedule_file = "config/schedule.yml"
   if File.exist?(schedule_file)
@@ -8,5 +8,5 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://localhost:6379/0" }
+  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
 end
